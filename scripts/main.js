cd37463053478ab75a1ee4c8b6fa738ebed054f3
ariqhikari <ahidayatbia@gmail.com>
2024-10-07 20:42:18 +0700

    feat: implement API predict image

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,62 @@
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  const elementHeight = rect.bottom - rect.top;
+  const elementWidth = rect.right - rect.left;
+
+  const visibleHeight = Math.max(
+    0,
+    Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0)
+  );
+  const visibleWidth = Math.max(
+    0,
+    Math.min(rect.right, windowWidth) - Math.max(rect.left, 0)
+  );
+
+  const isVisible =
+    (visibleHeight * visibleWidth) / (elementHeight * elementWidth) >= 0.25;
+
+  return isVisible;
+}
+
 function coverImages() {
   const divs = document.getElementsByTagName("div");
   const images = document.getElementsByTagName("img");
 
-  for (let div of divs) {
-    const hasBackgroundImage =
-      window.getComputedStyle(div).backgroundImage !== "none";
+  const urlRegex = /(url\(['"]?)([^'")]+)(['"]?\))/; // Regular expression to match URLs in the format url('...') or url("...")
 
-    if (hasBackgroundImage) {
-      div.style.filter = "blur(64px)";
+  for (let div of divs) {
+    const backgroundImage = window.getComputedStyle(div).backgroundImage;
+
+    const match = backgroundImage.match(urlRegex);
+    if (match && match[2]) {
+      const url = match[2]; // Extract the URL
+
+      const img = new Image();
+      img.src = url;
+
+      img.onload = function () {
+        const width = img.width;
+        const height = img.height;
+        if (width > 100 && height > 100) {
+          if (isElementInViewport(div)) {
+            div.style.filter = "blur(64px)";
+          }
+        }
+      };
+
+      img.onerror = function () {
+        console.error("Gagal memuat gambar latar belakang:", url);
+      };
     }
   }
 
   for (let img of images) {
-    const imgWidth = img.naturalWidth;
-    const imgHeight = img.naturalHeight;
+    const imgWidth = img.width;
+    const imgHeight = img.height;
 
     if (
       !img.dataset.covered &&
@@ -22,101 +65,159 @@ function coverImages() {
       !img.src.includes("logo") &&
       !img.src.includes("icon")
     ) {
-      const overlay = document.createElement("div");
-      overlay.style.position = "absolute";
-      overlay.style.top = "0";
-      overlay.style.left = "0";
-      overlay.style.width = "100%";
-      overlay.style.height = "100%";
-      overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-      overlay.style.color = "white";
-      overlay.style.display = "flex";
-      overlay.style.flexDirection = "column";
-      overlay.style.justifyContent = "center";
-      overlay.style.alignItems = "center";
-      overlay.style.fontSize = "16px";
-      overlay.style.textAlign = "center";
-      overlay.style.gap = "10px";
-      overlay.style.zIndex = "1000";
+      if (isElementInViewport(img)) {
+        const overlay = document.createElement("div");
+        overlay.style.position = "absolute";
+        overlay.style.top = `${img.offsetTop}px`;
+        overlay.style.left = `${img.offsetLeft}px`;
+        overlay.style.width = `${imgWidth}px`;
+        overlay.style.height = `${imgHeight}px`;
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        overlay.style.color = "white";
+        overlay.style.display = "flex";
+        overlay.style.flexDirection = "column";
+        overlay.style.justifyContent = "center";
+        overlay.style.alignItems = "center";
+        overlay.style.fontSize = "16px";
+        overlay.style.textAlign = "center";
+        overlay.style.zIndex = "999999";
 
-      img.style.filter = "blur(64px)";
-      img.style.opacity = "0.7";
+        img.style.filter = "blur(64px)";
+        img.style.opacity = "0.7";
 
-      const sensitiveText = document.createElement("div");
-      sensitiveText.innerText = "Image Content";
-      sensitiveText.style.fontSize = "18px";
-      sensitiveText.style.fontWeight = "bold";
-      overlay.appendChild(sensitiveText);
+        const sensitiveText = document.createElement("div");
+        sensitiveText.innerText = "Image Content";
+        sensitiveText.style.fontSize = "18px";
+        sensitiveText.style.fontWeight = "bold";
+        overlay.appendChild(sensitiveText);
 
-      const description = document.createElement("div");
-      description.innerText =
-        "This photo may contain sensitive or disturbing content.";
-      description.style.fontSize = "12px";
-      overlay.appendChild(description);
+        const description = document.createElement("div");
+        description.innerText =
+          "This image may contain sensitive or disturbing content.";
+        description.style.fontSize = "12px";
+        overlay.appendChild(description);
 
-      const loading = document.createElement("div");
-      loading.innerText = "Loading...";
-      loading.style.display = "none";
-      overlay.appendChild(loading);
+        const loading = document.createElement("div");
+        loading.innerText = "Loading...";
+        loading.style.display = "none";
+        overlay.appendChild(loading);
 
-      const button = document.createElement("button");
-      button.innerText = "See Image";
-      button.style.marginTop = "10px";
-      button.style.padding = "5px 15px";
-      button.style.backgroundColor = "transparent";
-      button.style.color = "white";
-      button.style.border = "1px solid white";
-      button.style.borderRadius = "5px";
-      button.style.cursor = "pointer";
-      button.onclick = function () {
-        loading.style.display = "block";
-        button.disabled = true;
+        const button = document.createElement("button");
+        button.innerText = "See Image";
+        button.style.marginTop = "10px";
+        button.style.padding = "5px 15px";
+        button.style.backgroundColor = "transparent";
+        button.style.color = "white";
+        button.style.border = "1px solid white";
+        button.style.borderRadius = "5px";
+        button.style.cursor = "pointer";
+        button.onclick = function (e) {
+          e.stopPropagation();
+          e.preventDefault();
 
-        setTimeout(() => {
-          alert("Success Fetch (simulation).");
-          overlay.remove();
-          img.style.filter = "none";
-          img.style.opacity = "1";
-          img.dataset.covered = "true";
-          img.style.pointerEvents = "auto";
-        }, 250);
-      };
+          loading.style.display = "block";
+          button.disabled = true;
 
-      overlay.appendChild(button);
-      img.style.position = "relative";
-      img.parentNode.insertBefore(overlay, img);
-      img.style.pointerEvents = "none";
+          fetch("https://flask-snailly.unikomcodelabs.id/predict-image", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              image_url: img.src,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("API response:", data);
+              if (data.hasil === "Berbahaya") {
+                img.src =
+                  "https://ariqhikari.github.io/proxy/assets/blocked-image.png";
+              }
 
-      img.dataset.covered = "false";
+              overlay.remove();
+              img.style.filter = "none";
+              img.style.opacity = "1";
+              img.dataset.covered = "true";
+            })
+            .catch((error) => {
+              console.error("Error fetching API:", error);
+            });
+        };
+
+        overlay.appendChild(button);
+
+        img.parentNode.insertBefore(overlay, img);
+        img.style.pointerEvents = "none";
+
+        img.dataset.covered = "false";
+      }
     }
   }
 }
 
-let isCoverImagesRunning = false; // Flag untuk melacak status coverImages
+// Fungsi lainnya tetap sama
 
-function handleInteraction() {
-  if (isCoverImagesRunning) return; // Cegah pemanggilan jika sudah berjalan
+function pauseAllVideos() {
+  const videos = document.getElementsByTagName("video");
 
-  isCoverImagesRunning = true; // Tandai fungsi sedang berjalan
-  coverImages();
-  isCoverImagesRunning = false; // Reset flag setelah selesai
+  for (let video of videos) {
+    console.log("URL VIDEO:", video.currentSrc || video.src);
+    // video.pause();
+  }
 }
 
-coverImages();
+let isCoverImagesRunning = false;
+let isPauseVideosRunning = false;
 
-const events = [
-  "scroll",
-  "click",
-  "keydown",
-  "mousemove",
-  "touchstart",
-  "touchmove",
-  "touchend", // Tambahkan jika perlu
-];
+function handleInteractionImage() {
+  if (isCoverImagesRunning) return;
 
-events.forEach((event) => {
-  window.addEventListener(event, () => {
-    clearTimeout(window.interactionTimeout);
-    window.interactionTimeout = setTimeout(handleInteraction, 100); // Panggil handleInteraction
+  isCoverImagesRunning = true;
+  coverImages();
+  isCoverImagesRunning = false;
+}
+
+function handleInteractionVideo() {
+  if (isPauseVideosRunning) return;
+
+  isPauseVideosRunning = true;
+  pauseAllVideos();
+  isPauseVideosRunning = false;
+}
+
+if (!window.location.href.includes("tiktok.com")) {
+  const events = [
+    "scroll",
+    "click",
+    "keydown",
+    "mousemove",
+    "touchstart",
+    "touchmove",
+    "touchend",
+  ];
+
+  events.forEach((event) => {
+    window.addEventListener(event, () => {
+      clearTimeout(window.interactionTimeout);
+      window.interactionTimeout = setTimeout(handleInteractionImage, 100);
+    });
   });
+}
+
+pauseAllVideos();
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === "childList") {
+      console.log("New elements added to the DOM");
+      clearTimeout(window.interactionTimeout);
+      window.interactionTimeout = setTimeout(handleInteractionVideo, 100);
+    }
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
 });
